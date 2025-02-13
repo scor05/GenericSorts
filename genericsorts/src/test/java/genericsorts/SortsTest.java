c3394e671a478edf01c4b72be4bb3324b762aa51
Santiago Cordero <santicordero28@gmail.com>
2025-02-13 17:11:48 -0600

    Agregado más casos de prueba para los JUnit.

--- genericsorts/src/test/java/genericsorts/SortsTest.java
+++ genericsorts/src/test/java/genericsorts/SortsTest.java
@@ -7,32 +7,96 @@ import org.junit.jupiter.api.Test;
 public class SortsTest {
 
     @Test
-    public void shouldReturnMergeSortedArray() {
-        Integer[] array = {5, 3, 1, 4, 2};
+    public void shouldReturnMergeSortedArrays() {
+        // Probar cinco casos: array vacía, array de un elemento, array ya ordenada, array desordenada y array de elementos repetidos
+        Integer[] arrayUnordered = {5, 3, 1, 4, 2};
+        Integer[] arrayOrdered = {1, 2, 3, 4, 5};
+        Integer[] arrayRepeated = {1, 1, 2, 2, 3};
+        Integer[] arrayEmpty = {};
+        Integer[] arraySingle = {1};
+
+        // Array desordenada y ordenada a la vez
         MergeSort<Integer> mergeSort = new MergeSort<>();
-        Integer[] sortedArray = mergeSort.sort(array);
-        for (int i = 0; i < array.length; i++) {
-            assertTrue(sortedArray[i] == (i+1));
+        Integer[] sortedUnorderedArray = mergeSort.sort(arrayUnordered);
+        Integer[] sortedOrderedArray = mergeSort.sort(arrayOrdered);
+        for (int i = 0; i < arrayUnordered.length; i++) {
+            assertTrue(sortedUnorderedArray[i] == (i+1));
+            assertTrue(sortedOrderedArray[i] == arrayOrdered[i]);
         }
+
+        // Repetida
+        Integer[] sortedRepeatedArray = mergeSort.sort(arrayRepeated);
+        assertTrue(sortedRepeatedArray[0] == 1);
+        assertTrue(sortedRepeatedArray[1] == 1);
+        assertTrue(sortedRepeatedArray[2] == 2);
+        assertTrue(sortedRepeatedArray[3] == 2);
+        assertTrue(sortedRepeatedArray[4] == 3);
+
+        // Array vacía y única
+        Integer[] sortedEmptyArray = mergeSort.sort(arrayEmpty);
+        Integer[] sortedSingleArray = mergeSort.sort(arraySingle);
+        assertTrue(sortedEmptyArray.length == 0);
+        assertTrue(sortedSingleArray.length == 1);
+        assertTrue(sortedSingleArray[0] == 1);  
     }
 
     @Test
-    public void shouldReturnInsertionSortedArray() {
-        Integer[] array = {5, 4, 1, 2, 3};
+    public void shouldReturnInsertionSortedArrays() {
+        Integer[] arrayUnordered = {5, 3, 1, 4, 2};
+        Integer[] arrayOrdered = {1, 2, 3, 4, 5};
+        Integer[] arrayRepeated = {1, 1, 2, 2, 3};
+        Integer[] arrayEmpty = {};
+        Integer[] arraySingle = {1};
+
         InsertionSort<Integer> insertionSort = new InsertionSort<>();
-        Integer[] sortedArray = insertionSort.sort(array);
-        for (int i = 0; i < array.length; i++) {
-            assertTrue(sortedArray[i] == (i+1));
+        Integer[] sortedUnorderedArray = insertionSort.sort(arrayUnordered);
+        Integer[] sortedOrderedArray = insertionSort.sort(arrayOrdered);
+        for (int i = 0; i < arrayUnordered.length; i++) {
+            assertTrue(sortedUnorderedArray[i] == (i+1));
+            assertTrue(sortedOrderedArray[i] == arrayOrdered[i]);
         }
+
+        Integer[] sortedRepeatedArray = insertionSort.sort(arrayRepeated);
+        assertTrue(sortedRepeatedArray[0] == 1);
+        assertTrue(sortedRepeatedArray[1] == 1);
+        assertTrue(sortedRepeatedArray[2] == 2);
+        assertTrue(sortedRepeatedArray[3] == 2);
+        assertTrue(sortedRepeatedArray[4] == 3);
+
+        Integer[] sortedEmptyArray = insertionSort.sort(arrayEmpty);
+        Integer[] sortedSingleArray = insertionSort.sort(arraySingle);
+        assertTrue(sortedEmptyArray.length == 0);
+        assertTrue(sortedSingleArray.length == 1);
+        assertTrue(sortedSingleArray[0] == 1);  
     }
 
     @Test
     public void shouldReturnRadixSortedArray(){
-        Integer[] array = {4, 5, 1, 3, 2};
+        Integer[] arrayUnordered = {5, 3, 1, 4, 2};
+        Integer[] arrayOrdered = {1, 2, 3, 4, 5};
+        Integer[] arrayRepeated = {1, 1, 2, 2, 3};
+        Integer[] arrayEmpty = {};
+        Integer[] arraySingle = {1};
+
         RadixSort<Integer> radixSort = new RadixSort<>(Integer::intValue);
-        Integer[] sortedArray = radixSort.sort(array);
-        for (int i = 0; i < array.length; i++) {
-            assertTrue(sortedArray[i] == (i+1));
+        Integer[] sortedUnorderedArray = radixSort.sort(arrayUnordered);
+        Integer[] sortedOrderedArray = radixSort.sort(arrayOrdered);
+        for (int i = 0; i < arrayUnordered.length; i++) {
+            assertTrue(sortedUnorderedArray[i] == (i+1));
+            assertTrue(sortedOrderedArray[i] == arrayOrdered[i]);
         }
+
+        Integer[] sortedRepeatedArray = radixSort.sort(arrayRepeated);
+        assertTrue(sortedRepeatedArray[0] == 1);
+        assertTrue(sortedRepeatedArray[1] == 1);
+        assertTrue(sortedRepeatedArray[2] == 2);
+        assertTrue(sortedRepeatedArray[3] == 2);
+        assertTrue(sortedRepeatedArray[4] == 3);
+
+        Integer[] sortedEmptyArray = radixSort.sort(arrayEmpty);
+        Integer[] sortedSingleArray = radixSort.sort(arraySingle);
+        assertTrue(sortedEmptyArray.length == 0);
+        assertTrue(sortedSingleArray.length == 1);
+        assertTrue(sortedSingleArray[0] == 1);  
     }
 }

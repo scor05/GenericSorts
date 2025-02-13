50ada1af8490640466bd6376ec47797ad1a235e1
Santiago Cordero <santicordero28@gmail.com>
2025-02-12 21:48:10 -0600

    Implementación de profiler en programa driver

--- genericsorts/src/main/java/genericsorts/Driver.java
+++ genericsorts/src/main/java/genericsorts/Driver.java
@@ -1,44 +1,65 @@
 package genericsorts;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
-
 public class Driver {
     public static void main(String[] args) {
         Random random = new Random();
-        int arraySize = random.nextInt(3000-10+1) + 10;
-        Integer[] randomArray = new Integer[arraySize]; // Array de números aleatorios para probar los algorítmos
-        for (int i = 0; i < randomArray.length; i++){
-            randomArray[i] = random.nextInt(3000);
+        int[] arraySizes = {10, 50, 100, 250, 500, 1000, 2000, 3000};
+        HashMap<Integer[], Integer> randomArrays = new HashMap<>(); // HashMap con array[] al azar y su tamaño respectivo.
+        for (int size : arraySizes) {
+            Integer[] randomArray = new Integer[size];
+            for (int i = 0; i < randomArray.length; i++){
+                randomArray[i] = random.nextInt(5000) + 1;
+            }
+            randomArrays.put(randomArray, size);
         }
-        /**
-         * Clave de posiciones de sortedArrays:
-         * 0: Quicksort
-         * 1: Mergesort
-         * 2: Insertionsort
-         * 3: Radixsort
-         * 4: Cyclesort
-         */
-        ArrayList<Integer[]> sortedArrays = new ArrayList<>();
-        MergeSort<Integer> mergeSort = new MergeSort<>();
-        InsertionSort<Integer> insertionSort = new InsertionSort<>();
-        // QuickSort<Integer> quickSort = new QuickSort<>();
-        // RadixSort<Integer> radixSort = new RadixSort<>();
-        // CycleSort<Integer> cycleSort = new CycleSort<>();
-        sortedArrays.add(mergeSort.sort(randomArray));
-        sortedArrays.add(insertionSort.sort(randomArray));
-        // sortedArrays.add(quickSort.sort(randomArray));
-        // sortedArrays.add(radixSort.sort(randomArray));
-        // sortedArrays.add(cycleSort.sort(randomArray));
-        System.out.println("El tiempo transcurrido por cada algorítmo es:");
-        String[] sortNames = {"Quicksort", "Mergesort", "Insertionsort", "Radixsort", "Cyclesort"};
-        for (int i = 0; i < sortNames.length; i++) {
-            System.out.println(sortNames[i] + ": " + sortedArrays.get(i)[0]);
-        }
-        System.out.println("Tamaño del array: " + arraySize);
-    }
 
+        HashMap<Integer[], Integer> randomSortedArrays = randomArrays;
+        MergeSort<Integer> mergeSort = new MergeSort<>(); // Este sort es únicamente utilizado para tener las arrays ordenadas y probar el peor caso de los algorítmos
+        for (Integer[] array : randomSortedArrays.keySet()) {
+            mergeSort.sort(array);
+        }
+
+        ArrayList<IGenericSort<Integer>> sorts = new ArrayList<>(); // TODO: Agregar Cycle y Quicksort
+        sorts.add(new InsertionSort<Integer>());
+        sorts.add(new MergeSort<Integer>());
+        sorts.add(new RadixSort<Integer>(Integer::intValue));
+        
+        for (IGenericSort<Integer> sort : sorts) {
+            System.out.println("\n" + "-".repeat(50));
+            System.out.println(sort.getClass().getSimpleName());
+            System.out.println("------------- Arrays no ordenados ----------------");
+            for (Integer[] array : randomArrays.keySet()) {
+                System.out.println("Tamaño del array: " + randomArrays.get(array));
+                System.out.println("Tiempo transcurrido en segundos: " + profiler(sort.getClass().getSimpleName(), new Runnable() {
+                    @Override
+                    public void run() {
+                        sort.sort(array);
+                    }
+                }));
+            }
+
+            System.out.println("------------- Arrays ya ordenados ----------------");
+            for (Integer[] array : randomSortedArrays.keySet()) {
+                System.out.println("Tamaño del array: " + randomSortedArrays.get(array));
+                System.out.println("Tiempo transcurrido en segundos: " + profiler(sort.getClass().getSimpleName(), new Runnable() {
+                    @Override
+                    public void run() {
+                        sort.sort(array);
+                    }
+                }));
+            }
+        }
+    }   
     
+    public static double profiler(String name, Runnable runnable) {
+        long startTime = System.nanoTime();
+        runnable.run();
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / Math.pow(10,9); // retorna en segundos el tiempo (x10^9)
+    }
 }
 
 

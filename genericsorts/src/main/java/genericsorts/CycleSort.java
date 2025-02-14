72127ef5a0bd70f4115eb2739cd9f3e2d151d161
DeividArriaza <lop24730@uvg.edu.gt>
2025-02-13 19:57:38 -0600

    Se ha agregado un if para retornar arreglos que sean de 0 o no contengan nada

--- genericsorts/src/main/java/genericsorts/CycleSort.java
+++ genericsorts/src/main/java/genericsorts/CycleSort.java
@@ -1,5 +1,10 @@
 package genericsorts;
 
-public class CycleSort {
-    
+public class CycleSort<T extends Comparable<T>>implements IGenericSort<T> {
+    @Override
+    public T[] sort(T[] array) {
+        if (array == null || array.length == 0) {
+             return array;
+        }
+    }
 }

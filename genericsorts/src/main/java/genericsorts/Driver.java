34c92eb1219a92c9509a942ee1444141895aa0fc
Santiago Cordero <santicordero28@gmail.com>
2025-02-13 22:56:53 -0600

    Corregido un error en el Driver que causaba que al establecer las arrays de randomSortedArrays solo se estaban cambiando las de la referencia a randomArrays.

--- genericsorts/src/main/java/genericsorts/Driver.java
+++ genericsorts/src/main/java/genericsorts/Driver.java
@@ -1,6 +1,7 @@
 package genericsorts;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Random;
 public class Driver {
@@ -16,13 +17,16 @@ public class Driver {
             randomArrays.put(randomArray, size);
         }
 
-        HashMap<Integer[], Integer> randomSortedArrays = randomArrays;
+        HashMap<Integer[], Integer> randomSortedArrays = new HashMap<>(); // HashMap con array[] al azar y su tamaño respectivo.
         MergeSort<Integer> mergeSort = new MergeSort<>(); // Este sort es únicamente utilizado para tener las arrays ordenadas y probar el peor caso de los algorítmos
-        for (Integer[] array : randomSortedArrays.keySet()) {
+        for (Integer[] array : randomArrays.keySet()) {
+            Integer[] sortedCopy = Arrays.copyOf(array, array.length);
             mergeSort.sort(array);
+            randomSortedArrays.put(sortedCopy, randomArrays.get(array));
         }
+        
 
-        ArrayList<IGenericSort<Integer>> sorts = new ArrayList<>(); // TODO: Agregar Cycle y Quicksort
+        ArrayList<IGenericSort<Integer>> sorts = new ArrayList<>();
         sorts.add(new InsertionSort<Integer>());
         sorts.add(new MergeSort<Integer>());
         sorts.add(new RadixSort<Integer>(Integer::intValue));
@@ -34,34 +38,34 @@ public class Driver {
             System.out.println(sort.getClass().getSimpleName());
             System.out.println("------------- Arrays no ordenados ----------------");
             for (Integer[] array : randomArrays.keySet()) {
+                Integer[] arrayToSort = Arrays.copyOf(array, array.length);
                 System.out.println("Tamaño del array: " + randomArrays.get(array));
-                System.out.println("Tiempo transcurrido en segundos: " + profiler(sort.getClass().getSimpleName(), new Runnable() {
+                System.out.println("Tiempo transcurrido en milisegundos: " + profiler(new Runnable() {
                     @Override
                     public void run() {
-                        sort.sort(array);
+                        sort.sort(arrayToSort);
                     }
                 }));
             }
 
             System.out.println("------------- Arrays ya ordenados ----------------");
             for (Integer[] array : randomSortedArrays.keySet()) {
+                Integer[] arrayToSort = Arrays.copyOf(array, array.length);
                 System.out.println("Tamaño del array: " + randomSortedArrays.get(array));
-                System.out.println("Tiempo transcurrido en segundos: " + profiler(sort.getClass().getSimpleName(), new Runnable() {
+                System.out.println("Tiempo transcurrido en milisegundos: " + profiler(new Runnable() {
                     @Override
                     public void run() {
-                        sort.sort(array);
+                        sort.sort(arrayToSort);
                     }
                 }));
             }
         }
     }   
     
-    public static double profiler(String name, Runnable runnable) {
+    public static double profiler(Runnable runnable) {
         long startTime = System.nanoTime();
         runnable.run();
         long endTime = System.nanoTime();
-        return (endTime - startTime) / Math.pow(10,9); // retorna en segundos el tiempo (x10^9)
+        return (endTime - startTime) / Math.pow(10,3); // retorna en milisegundos el tiempo
     }
 }
-
-

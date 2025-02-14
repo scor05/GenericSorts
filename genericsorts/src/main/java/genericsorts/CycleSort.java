925bb2fad8f7e7bd25af1f28da3b0939bdc6b394
DeividArriaza <lop24730@uvg.edu.gt>
2025-02-13 20:45:42 -0600

    Se ha terminado la clase CycleSort con todas sus iteraciones

--- genericsorts/src/main/java/genericsorts/CycleSort.java
+++ genericsorts/src/main/java/genericsorts/CycleSort.java
@@ -1,10 +1,54 @@
 package genericsorts;
 
-public class CycleSort<T extends Comparable<T>>implements IGenericSort<T> {
+public class CycleSort<T extends Comparable<T>> implements IGenericSort<T> {
     @Override
     public T[] sort(T[] array) {
         if (array == null || array.length == 0) {
-             return array;
+            return array;
         }
+        
+        int n = array.length;
+        //Se it
+        for (int cycleStart = 0; cycleStart < n - 1; cycleStart++) {
+            T item = array[cycleStart];
+            int pos = cycleStart;
+            
+            for (int i = cycleStart + 1; i < n; i++) {
+                if (array[i].compareTo(item) < 0) {
+                    pos++;
+                }
+            }
+            
+            if (pos == cycleStart) {
+                continue;
+            }
+            
+            while (item.equals(array[pos])) {
+                pos++;
+            }
+            
+            T temp = array[pos];
+            array[pos] = item;
+            item = temp;
+            
+            while (pos != cycleStart) {
+                pos = cycleStart;
+                
+                for (int i = cycleStart + 1; i < n; i++) {
+                    if (array[i].compareTo(item) < 0) {
+                        pos++;
+                    }
+                }
+                
+                while (item.equals(array[pos])) {
+                    pos++;
+                }
+                
+                temp = array[pos];
+                array[pos] = item;
+                item = temp;
+            }
+        }
+        return array;
     }
 }
